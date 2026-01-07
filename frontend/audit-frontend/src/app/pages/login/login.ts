f94240cb8bccdc2af6209ef0769051fbe6bce9ae
Sai <tomato.202504.tsoimk@gmail.com>
2026-01-07 17:07:29 +0900

    login frontend

--- frontend/audit-frontend/src/app/pages/login/login.ts
+++ frontend/audit-frontend/src/app/pages/login/login.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  submitted = signal(false);
+  loginForm!: FormGroup;
+  loading = signal(false);
+  errorMessage = signal('');
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+
+    // 用户一输入，就自动清除该字段错误
+    this.loginForm.valueChanges.subscribe(() => {
+      if (this.submitted()) {
+        // 重新触发模板计算
+        this.submitted.set(true);
+      }
+    });
+  }
+
+  onSubmit() {
+    this.submitted.set(true);
+
+    if (this.loginForm.invalid || this.loading()) {
+      return;
+    }
+
+    this.loading.set(true);
+    this.errorMessage.set('');
+
+    const { username, password } = this.loginForm.value;
+
+    console.log('submit:', username, password);
+
+    // backendが完成するまで一旦mock
+    setTimeout(() => {
+      if (username === 'admin' && password === '123456') {
+        localStorage.setItem('token', 'fake-token');
+        console.log('login success');
+      } else {
+        this.errorMessage.set('ユーザーIDまたはパスワードが間違っています。');
+      }
+      this.loading.set(false);
+    }, 1000)
+  }
+
+  // 关键方法：是否显示必填错误
+  showRequired(controlName: 'username' | 'password'): boolean {
+    const control = this.loginForm.get(controlName);
+    return (
+      this.submitted() &&
+      !!control &&
+      control.hasError('required')
+    );
+  }
 }
