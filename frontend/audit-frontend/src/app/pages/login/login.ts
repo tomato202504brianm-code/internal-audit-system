79eee27fba2e42900aa264ba9b0f1319ba8d11b7
Sai <tomato.202504.tsoimk@gmail.com>
2026-01-08 15:38:18 +0900

    correction

--- frontend/audit-frontend/src/app/pages/login/login.ts
+++ frontend/audit-frontend/src/app/pages/login/login.ts
@@ -60,7 +60,7 @@ export class Login {
 
     this.authService.login(username, password).subscribe({
       next: (res) => {
-        console.log(res.body.user.username);
+        console.log(res.user.username);
         console.log(res.message);
         this.onLoginSuccess();
       },
