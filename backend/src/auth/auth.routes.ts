a92bc57a22da1b91e01ecc165b5106cb4d63bf87
ブライアン <tomato.202504.brianm@gmail.com>
2026-01-08 15:36:50 +0900

    add JWT

--- backend/src/auth/auth.routes.ts
+++ backend/src/auth/auth.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { hashPassword, comparePassword } from "../utils/password";
 import { findUserByUsername } from '../repositories/user.repository';
+import jwt from 'jsonwebtoken';
+
+
 
 
 const router = Router();
@@ -8,12 +11,15 @@ const router = Router();
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
+
     const user = await findUserByUsername(username);
     if (!user) {
         return res.status(401).json({
-            message: 'ユーザーが見つかりません',
+            message: 'ユーザー名またはパスワードが見つかりません',
         });
     }
+
+
 
     // 2. Compare the password
     const isMatch =await comparePassword(password, user.password_hash);
@@ -25,11 +31,15 @@ router.post('/login', async (req, res) => {
         });
     }
 
+    const token = jwt.sign({ username: user.username },process.env.JWT_SECRET!, { expiresIn: '1h' });
+
+
     // 4. Success Path: If we reach this line, both checks passed.
     // We use status 200 (OK) for successful login.
     return res.status(200).json({
-        message: 'ログイン成功',
-        user: { username }
+        message: 'ログイン成功', 
+        user: { username },
+        token
     });
 });
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 const app = require('./app').default;
 
 const PORT = 50000;
